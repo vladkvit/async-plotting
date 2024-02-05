5e9a12a7688e581996e518b4900aa13ec27826c1
igorbasko01 <igorbasko@gmail.com>
2024-02-05 20:31:28 +0200

    generate random numbers in the plot

--- publisher/publish.py
+++ publisher/publish.py
@@ -1,13 +1,17 @@
+import random
+
 import zmq
 import aiozmq
 import asyncio
+
 
 async def publish_messages():
     publisher = await aiozmq.create_zmq_stream(zmq.PUB, bind='tcp://*:5556')
 
     counter = 0
     while counter < 100:
-        message = f'{counter}'.encode('utf-8')
+        random_number = random.randint(0, 100)
+        message = f'{random_number}'.encode('utf-8')
         publisher.write([b'topic', message])
         print(f'Published: {message}')
         await asyncio.sleep(1)
